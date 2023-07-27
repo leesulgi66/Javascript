a7bc9dc08bfc8c07111f9e552628e8b83c5fdfff
sulgi <leesulgi66@naver.com>
2023-07-27 21:37:03 +0900

    Functions part Two

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-function sayHello() {
-    console.log("Hello!");
+function plus(a, b) {
+    console.log(a + b);
 }
 
-sayHello();
-sayHello();
-sayHello();
-sayHello();
+function divide(a, b) {
+    console.log(a / b);
+}
+
+plus(8, 60);
+divide(40, 8);
+
+console.log("--------------------------------");
+
+// --------------------------------------------------
+
+const player = {
+    name : "sulgi", 
+    sayHello : function(otherPersonsName) {
+        console.log("hello! "+otherPersonsName + " nice to meet you");
+    }
+}
+
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
