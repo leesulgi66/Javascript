467ac63831170da230a6b5d45007351b69956577
sulgi <leesulgi66@naver.com>
2023-07-27 19:12:00 +0900

    boolean

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "sulgi";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "leesulgi";
-
-console.log("your new name is "+myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
