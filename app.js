3b36f8a96422541ae804e9fb4c969e5602fbfaab
sulgi <leesulgi66@naver.com>
2023-07-27 19:32:45 +0900

    Arrays

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const daysOfWeek = ["mon","tue","wed","thu","fri","sat"];
+
+console.log(daysOfWeek[0]);
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun")
+
+console.log(daysOfWeek);
