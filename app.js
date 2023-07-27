795d9c423692b50db39530d781d2aa2fa2c03db9
sulgi <leesulgi66@naver.com>
2023-07-27 20:55:32 +0900

    Objects

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const daysOfWeek = ["mon","tue","wed","thu","fri","sat"];
+const player = {
+    name:"sulgi", 
+    points: 10,
+    fat: true,
 
-console.log(daysOfWeek[0]);
+};
+console.log(player);
+console.log(player.name);
 
-// Get Item from Array
-console.log(daysOfWeek);
+player.name = "nico";
 
-// Add one more day to the array
-daysOfWeek.push("sun")
+console.log(player["name"]);
 
-console.log(daysOfWeek);
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player);
