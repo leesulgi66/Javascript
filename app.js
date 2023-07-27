ec25aec09f0b6ff7f2e1c09eb8d9e12aff22db06
sulgi <leesulgi66@naver.com>
2023-07-27 21:10:33 +0900

    Functions part One

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-const player = {
-    name:"sulgi", 
-    points: 10,
-    fat: true,
+function sayHello() {
+    console.log("Hello!");
+}
 
-};
-console.log(player);
-console.log(player.name);
-
-player.name = "nico";
-
-console.log(player["name"]);
-
-player.lastName = "potato";
-player.points = player.points + 15;
-console.log(player);
+sayHello();
+sayHello();
+sayHello();
+sayHello();
