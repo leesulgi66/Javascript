d5a55bc199718a19a635f3481341143307a41c3a
sulgi <leesulgi66@naver.com>
2023-07-29 16:12:41 +0900

    Searching For Elements

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const title = document.getElementById("title");
+const hellos = document.getElementsByClassName("hello");
+let title = document.getElementsByTagName("h1");
 
-title.innerText = "Got you!";
+title = document.getElementById("hello");
+title = document.querySelector("#hello"); // 위의 코드와 작동이 완전히 같다.
 
-console.log(title.id);
-console.log(title.className);
+title = document.querySelector(".hello h1");
+title = document.querySelector("div h2"); // null
+title = document.querySelector("div h1");
+title = document.querySelectorAll("div h1");
+console.log(title);
