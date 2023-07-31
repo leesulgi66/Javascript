0e90eb8bbcc871848de658023f16c39526b4e1a3
sulgi <leesulgi66@naver.com>
2023-07-31 16:07:21 +0900

    More Events

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const title = document.querySelector("div.hello:first-child h1")
+const h1 = document.querySelector("div.hello:first-child h1")
 
-console.dir(title);
+console.dir(h1);
 
 function handleTitleClick() {
-    title.style.color = "blue";
+    h1.style.color = "blue";
     console.log("title was clicked");
 }
 
 function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
+    h1.innerText = "Mouse is here!";
 }
 
 function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOOOD");
 }
 
 
-title.addEventListener("click", handleTitleClick);  // 함수의 괄호()를 넣지 않아야 한다. 
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click" , handleTitleClick); 
+h1.addEventListener("mouseenter" ,  handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
