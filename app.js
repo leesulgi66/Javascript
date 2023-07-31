90c6ce17fcebb4ba671226bbabd3a5f7118d7ada
sulgi <leesulgi66@naver.com>
2023-07-31 15:45:19 +0900

    Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const title = document.querySelector("div.hello:first-child h1")
 
+console.dir(title);
+
 function handleTitleClick() {
     title.style.color = "blue";
     console.log("title was clicked");
 }
 
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
 
 title.addEventListener("click", handleTitleClick);  // 함수의 괄호()를 넣지 않아야 한다. 
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
