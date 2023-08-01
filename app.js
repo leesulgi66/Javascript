780e4d2b63cb1b75924db36528234673a0116113
sulgi <leesulgi66@naver.com>
2023-08-01 15:56:15 +0900

    Form Submission

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const loginInput = document.querySelector("#login-form, input");
 const loginButton = document.querySelector("#login-form, button");
 
 function onLoginBtnClick() {
-    console.log("Hello", loginInput.value);
+    const username = loginInput.value;
+    // if(username === "") {
+    //     alert("Please write your name");
+    // }else if(username.length > 15) {
+    //     alert("Your name is too long.");
+    // }
+    console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
