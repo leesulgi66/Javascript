72a8f0fe0727910d609dc013fbc62292ebe51752
sulgi <leesulgi66@naver.com>
2023-08-01 16:54:35 +0900

    Events

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const loginInput = document.querySelector("#login-form, input");
-const loginButton = document.querySelector("#login-form, button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function onLoginBtnClick() {
+function onLoginSubmit() { // JS의 event 함수는 첫번째 전달인자에 해당 event의 정보를 담아준다.
+    event.preventDefault(); // form은 누군가가 submit 하면 기본적으로 페이지를 새로고침 하도록 되어있음. 그 기능을 끄는 함수.
     const username = loginInput.value;
-    // if(username === "") {
-    //     alert("Please write your name");
-    // }else if(username.length > 15) {
-    //     alert("Your name is too long.");
-    // }
     console.log(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
