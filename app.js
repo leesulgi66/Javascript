4079ef3d3958c3cb0a39ca99a10461cfffa80429
sulgi <leesulgi66@naver.com>
2023-08-01 14:26:02 +0900

    CSS in Javascirpt part Three

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if(h1.className === clickedClass) {
-        h1.className = "";  
-    } else {
-        h1.className = clickedClass;
-    }
+    // const clickedClass = "clicked";
+    // if(h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
+    h1.classList.toggle("clicked");  // toggle을 이용해 위의 6줄을 대체할 수 있다.  
 }
 
 
