4dac8faf0bdb2cee5bb2e3406b3d73df1b54eab6
sulgi <leesulgi66@naver.com>
2023-08-01 20:37:52 +0900

    link preventDefault

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
-function onLoginSubmit() { // JS의 event 함수는 첫번째 전달인자에 해당 event의 정보를 담아준다.
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) { // JS의 event 함수는 첫번째 전달인자에 해당 event의 정보를 담아준다.
     event.preventDefault(); // form은 누군가가 submit 하면 기본적으로 페이지를 새로고침 하도록 되어있음. 그 기능을 끄는 함수.
     const username = loginInput.value;
     console.log(username);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+    console.dir("clicked");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
