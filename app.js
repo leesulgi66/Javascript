7ec297c56d86af09be2f6c0b870b792da2acce85
sulgi <leesulgi66@naver.com>
2023-08-01 15:07:27 +0900

    Input Value

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1")
+const loginInput = document.querySelector("#login-form, input");
+const loginButton = document.querySelector("#login-form, button");
 
-function handleTitleClick() {
-    // const clickedClass = "clicked";
-    // if(h1.classList.contains(clickedClass)) {
-    //     h1.classList.remove(clickedClass);
-    // } else {
-    //     h1.classList.add(clickedClass);
-    // }
-    h1.classList.toggle("clicked");  // toggle을 이용해 위의 6줄을 대체할 수 있다.  
+function onLoginBtnClick() {
+    console.log("Hello", loginInput.value);
 }
 
-
-h1.addEventListener("click" , handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
