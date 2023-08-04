8979f9099703a0a4ee9a862118d2eb9001145fd8
sulgi <leesulgi66@naver.com>
2023-08-04 16:32:08 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");  //  ===  const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    console.log(li);
+    li.remove();
+    // this.parentNode.remove();   <- 한줄로 대체 가능
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
