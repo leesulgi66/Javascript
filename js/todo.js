c269c8c27f84589a3662d801296c7244175bb031
sulgi <leesulgi66@naver.com>
2023-08-06 11:07:17 +0900

    Deleting To Dos Part One

--- js/todo.js
+++ js/todo.js
@@ -11,30 +11,35 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-    console.log(li);
+    console.log(li.id);
     li.remove();
-    // this.parentNode.remove();   <- 한줄로 대체 가능
 }
 
 function paintToDo(newTodo) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo;
-    const button = document.createElement("button");
+    const li = document.createElement("li");  // 리스트를 생성
+    li.id = newTodo.id;  // li의 아이디를 newTodoObj의 key값으로 설정
+    const span = document.createElement("span");  // span을 생성
+    span.innerText = newTodo.text;  // 게시내용을 newTodoObj의 value값으로 설정
+    const button = document.createElement("button");  // 버튼을 생성
     button.innerText = "❌";
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
+    button.addEventListener("click", deleteToDo);  // 버튼의 클릭 이벤트 설정
+    li.appendChild(span); // span을 리스트의 자식으로 만들어줌
+    li.appendChild(button); // 버튼을 리스트의 자식으로 만들어줌
+
+    toDoList.appendChild(li); // 생성 완료된 리스트를 최종적으로 ul의 자식으로 만들어줌.
 }
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
-    saveToDos();
+    const newTodoObj = {
+        text:newTodo, // 새로 작성된 toDo를 text로 넣어준 후
+        id: Date.now(), // 현재시간ms을 이용해 랜덤성 id를 생성해 지정해 준다.
+    }
+    toDos.push(newTodoObj); // localStorage에 Object로 저장
+    paintToDo(newTodoObj); // paintToDo에도 text로 넘겨 주는 것이 아니라 Objec로 넘겨준다. key,value
+    saveToDos(); 
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
