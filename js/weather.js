509cec192ce66760ee03e9256b29a429732c0eba
sulgi <leesulgi66@naver.com>
2023-08-06 20:21:09 +0900

    Annotation

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
-function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+function onGeoOk(position) { // 성공시
+    const lat = position.coords.latitude; // 위도
+    const lng = position.coords.longitude; // 경도
     console.log("You live in", lat, lng);
 }
 
@@ -8,4 +8,4 @@ function onGeoError() {
     alert("Can't find you. No weather for you.")
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);  // 현재 위치를 알려주는 함수. (성공함수, 실패함수) 두개의 인자를 받는다.
